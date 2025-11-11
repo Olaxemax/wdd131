2e174e14404ba0d55712c32917892da18763f953
olaxemax <eolalemi@byupathway.edu>
2025-11-11 20:20:16 +0100

    week02

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,19 +3,19 @@ const temples = [
     templeName: "Aba Nigeria Temple",
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-lds-253854-wallpaper.jpg"
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/7210c09be95c4474772ae52e5f31c23c08112784/full/500%2C/0/default"
   },
   {
     templeName: "Accra Ghana Temple",
     location: "Accra, Ghana",
     dedicated: "2004, January, 11",
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-lds-253855-wallpaper.jpg"
+    imageUrl: "https://assets.ldscdn.org/4e/59/4e599a8aaaf9071212496225584aaad08c91a7cc/accra_ghana_temple_lds.jpeg"
   },
   {
     templeName: "Johannesburg South Africa Temple",
     location: "Johannesburg, South Africa",
     dedicated: "1985, August, 24",
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/johannesburg-south-africa-temple/johannesburg-south-africa-temple-lds-253856-wallpaper.jpg"
+    imageUrl: "https://tse2.mm.bing.net/th/id/OIP.v3senR2n1Pbosl_DkQ2POQHaHa?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3"
   }
 ];
 
